8f492e146231ee83e76e2597f7de083fb96da1d3
neilellis <hello@neilellis.me>
2015-08-18 15:08:54 +0100

    Templated README

--- src/test/java/com/sillelien/dollar/DocTest.java
+++ src/test/java/com/sillelien/dollar/DocTest.java
@@ -1,3 +1,21 @@
+/*
+ *   Copyright (c) 2014-2015 Neil Ellis
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *
+ */
+
 package com.sillelien.dollar;
 
 import org.apache.commons.io.FileUtils;
@@ -18,7 +36,7 @@ public class DocTest {
     public void test() throws IOException {
         PegDownProcessor pegDownProcessor = new PegDownProcessor(Extensions.FENCED_CODE_BLOCKS);
         PegDownProcessor processor = new PegDownProcessor(Extensions.FENCED_CODE_BLOCKS);
-        RootNode rootNode = processor.parseMarkdown(FileUtils.readFileToString(new File("README.md")).toCharArray());
+        RootNode rootNode = processor.parseMarkdown(FileUtils.readFileToString(new File("README.tmpl.md")).toCharArray());
         rootNode.accept(new DocTestingVisitor());
 
     }
