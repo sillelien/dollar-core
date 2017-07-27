c10ccf4ae459419c7cb914f33cc2fdf8b87c908f
neilellis <neil@vizz.buzz>
2017-07-27 18:00:42 +0100

    Constrained vars

---
+++ src/main/java/com/sillelien/dollar/api/types/ConstrainedVar.java
@@ -0,0 +1,45 @@
+/*
+ *    Copyright (c) 2014-2017 Neil Ellis
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.sillelien.dollar.api.types;
+
+import com.sillelien.dollar.api.var;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ConstrainedVar extends DollarWrapper {
+    @NotNull
+    private final String constraintFingerPrint;
+
+    ConstrainedVar(@Nullable var value, @NotNull var constraint) {
+        super(value);
+        this.constraintFingerPrint = constraint.toDollarScript();
+    }
+
+    ConstrainedVar(@Nullable var value, @NotNull String constraint) {
+        super(value);
+        this.constraintFingerPrint = constraint;
+    }
+
+    @Override
+    public var $constrain(var constraint) {
+        if (constraint.toDollarScript().equals(constraintFingerPrint)) {
+            return super.$constrain(constraint);
+        } else {
+            throw new ConstraintViolation(this, constraint, constraintFingerPrint);
+        }
+    }
+}
