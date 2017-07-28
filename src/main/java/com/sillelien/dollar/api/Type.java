41c795905132947b992ef3f69840feccbbe9acf6
neilellis <neil@vizz.buzz>
2017-07-28 18:27:04 +0100

    Constrained vars

--- src/main/java/com/sillelien/dollar/api/Type.java
+++ src/main/java/com/sillelien/dollar/api/Type.java
@@ -25,56 +25,70 @@ public final class Type {
     /**
      * The constant STRING.
      */
+    @NotNull
     public static final Type _STRING = new Type("STRING");
     /**
      * The constant DECIMAL.
      */
+    @NotNull
     public static final Type _DECIMAL = new Type("DECIMAL");
     /**
      * The constant INTEGER.
      */
+    @NotNull
     public static final Type _INTEGER = new Type("INTEGER");
     /**
      * The constant LIST.
      */
+    @NotNull
     public static final Type _LIST = new Type("LIST");
     /**
      * The constant MAP.
      */
+    @NotNull
     public static final Type _MAP = new Type("MAP");
     /**
      * The constant URI.
      */
+    @NotNull
     public static final Type _URI = new Type("URI");
     /**
      * The constant VOID.
      */
+    @NotNull
     public static final Type _VOID = new Type("VOID");
     /**
      * The constant RANGE.
      */
+    @NotNull
     public static final Type _RANGE = new Type("RANGE");
     /**
      * The constant BOOLEAN.
      */
+    @NotNull
     public static final Type _BOOLEAN = new Type("BOOLEAN");
     /**
      * The constant ERROR.
      */
+    @NotNull
     public static final Type _ERROR = new Type("ERROR");
     /**
      * The constant DATE.
      */
+    @NotNull
     public static final Type _DATE = new Type("DATE");
     /**
      * The constant INFINITY.
      */
+    @NotNull
     public static final Type _INFINITY = new Type("INFINITY");
     /**
      * The constant ANY.
      */
+    @NotNull
     public static final Type _ANY = new Type("ANY");
 
+    @NotNull
     public static final Type _QUEUE = new Type("QUEUE");
 
 
@@ -87,7 +101,7 @@ public final class Type {
      *
      * @param name the name
      */
-    public Type(@NotNull String name, String constraint) {
+    public Type(@NotNull String name, @NotNull String constraint) {
 
         this.name = name.toUpperCase();
         this.constraint = constraint;
@@ -96,9 +110,8 @@ public final class Type {
     /**
      * Instantiates a new Type.
      *
-     * @param name the name
      */
-    public Type(@NotNull Type type, String constraint) {
+    public Type(@NotNull Type type, @NotNull String constraint) {
 
         this.name = type.name;
         this.constraint = constraint;
@@ -138,7 +151,7 @@ public final class Type {
     }
 
     public boolean is(@Nullable Type t) {
-        return name.equals(t.name);
+        return name.equals(t != null ? t.name :null);
 
     }
 
